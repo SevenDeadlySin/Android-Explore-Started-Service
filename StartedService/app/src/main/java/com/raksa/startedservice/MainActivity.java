cb0c4d615fabf73d36a455189a525cdd2a72844d
BamBo <raksameth@gmail.com>
2017-04-25 07:17:11 +0700

    Implement Code Blockk For Button Event and Overrider Method of Service

--- StartedService/app/src/main/java/com/raksa/startedservice/MainActivity.java
+++ StartedService/app/src/main/java/com/raksa/startedservice/MainActivity.java
@@ -1,5 +1,6 @@
 package com.raksa.startedservice;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,10 +14,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onButtonStart(View view) {
-
+        Intent startIntent = new Intent( MainActivity.this , MyStartedService.class);
+        startService(startIntent);
     }
 
     public void onButtonStop(View view) {
-
+        Intent stopIntent = new Intent( MainActivity.this, MyStartedService.class);
+        stopService(stopIntent);
     }
 }
--- StartedService/app/src/main/java/com/raksa/startedservice/MyStartedService.java
+++ StartedService/app/src/main/java/com/raksa/startedservice/MyStartedService.java
@@ -4,6 +4,7 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 /**
  * Created by Raksa on 4/22/2017.
@@ -11,25 +12,33 @@ import android.support.annotation.Nullable;
 
 public class MyStartedService extends Service {
 
+    private final String TAG = MyStartedService.class.getSimpleName();
+
 
     @Override
     public void onCreate() {
+
+        Log.i(TAG,"onCreate , Thread Name : "+Thread.currentThread().getName());
         super.onCreate();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(TAG,"onStartCommand , Thread Name : "+Thread.currentThread().getName());
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
+        Log.i(TAG,"onBind , Thread Name : "+Thread.currentThread().getName());
         return null;
     }
 
     @Override
     public void onDestroy() {
+        Log.i(TAG,"onDestroy , Thread Name : "+Thread.currentThread().getName());
         super.onDestroy();
     }
+
 }
